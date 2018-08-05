0af3c7e6556e453743d22ab9f8bda6c076defafd
Malgorzata Dominikowska <m.dominikowska@wp.pl>
2018-08-05 11:31:16 +0200

    settings

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My React Project';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
